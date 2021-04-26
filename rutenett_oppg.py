32c14a201307d1db8cd806e5c0ca28f06fc792f7
Frode Fjellanger <frode.fjellanger@vlfk.no>
2021-04-26 10:20:17 +0200

    Endra til å bruka funksjonar

--- rutenett_oppg.py
+++ rutenett_oppg.py
@@ -1,24 +1,29 @@
-# Laga eit rutenett med løkker (og kanskje funksjon).
+# Døme laga eit rutenett med løkker og funksjonar.
 
 import turtle as t
-t.hideturtle()
-t.speed(10)
-y=0
-t.fillcolor("yellow")
-t.begin_fill()
-for r in range(2):
-    for k in range(2):
-        for l in range (4):
-            t.forward(50)
-            t.left(90)
-        t.up()
-        t.forward(50)
-        t.down()
 
-    t.up()
-    y=y+50
-    t.goto(0,y)
-    t.down()
-    #t.write(t.pos())
-t.end_fill()
+def ei_rute():
+    t.forward(50)
+    t.left(90)
+
+def rutenett():
+    t.hideturtle()
+    t.speed(10)
+    y=0
+    t.fillcolor("yellow")
+    t.begin_fill()
+    for r in range(4):
+        for k in range(3):
+            for l in range (4):
+                ei_rute()
+            t.up()
+            t.forward(50)
+            t.down()
+        t.up()
+        y=y+50
+        t.goto(0,y)
+        t.down()
+    t.end_fill()
+
+rutenett()
 input()
